dccda249d7a39ae7642a93dd2539c9882499a403
cristiangpr <cristiangp777@gmail.com>
2024-03-27 10:51:46 -0600

    cleanup

--- index.js
+++ index.js
@@ -38,46 +38,6 @@ async function main() {
   console.log(`- The smart contract ID is: ${contractId} \n`);
   console.log(`- Smart contract ID in Solidity format: ${contractAddress} \n`);
 
-  /*Query the contract to check changes in state variable
-  const contractQueryTx = new ContractCallQuery()
-    .setContractId(contractId)
-    .setGas(100000)
-    .setFunction(
-      "getMobileNumber",
-      new ContractFunctionParameters().addString("Alice")
-    );
-  const contractQuerySubmit = await contractQueryTx.execute(client);
-  const contractQueryResult = contractQuerySubmit.getUint256(0);
-  console.log(
-    `- Here's the phone number you asked for: ${contractQueryResult} \n`
-  );
 
-  // Call contract function to update the state variable
-  const contractExecuteTx = new ContractExecuteTransaction()
-    .setContractId(contractId)
-    .setGas(100000)
-    .setFunction(
-      "setMobileNumber",
-      new ContractFunctionParameters().addString("Bob").addUint256(222222)
-    );
-  const contractExecuteSubmit = await contractExecuteTx.execute(client);
-  const contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
-  console.log(
-    `- Contract function call status: ${contractExecuteRx.status} \n`
-  );
-
-  // Query the contract to check changes in state variable
-  const contractQueryTx1 = new ContractCallQuery()
-    .setContractId(contractId)
-    .setGas(100000)
-    .setFunction(
-      "getMobileNumber",
-      new ContractFunctionParameters().addString("Bob")
-    );
-  const contractQuerySubmit1 = await contractQueryTx1.execute(client);
-  const contractQueryResult1 = contractQuerySubmit1.getUint256(0);
-  console.log(
-    `- Here's the phone number you asked for: ${contractQueryResult1} \n`
-  ); */
 }
 main();
